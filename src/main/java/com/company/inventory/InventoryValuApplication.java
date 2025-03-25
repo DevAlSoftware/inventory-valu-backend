8b81b41dc768c65890b0269eab263dee3aa46ca8
Alejandro Quintero Alzate <alejandro.quintero@robotmade.us>
2025-03-24 22:25:04 -0500

    feauture: Creacion del proyecto base para la creacion de sistema de gestión de inventarios

---
+++ src/main/java/com/company/inventory/InventoryValuApplication.java
@@ -0,0 +1,13 @@
+package com.company.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryValuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryValuApplication.class, args);
+	}
+
+}
