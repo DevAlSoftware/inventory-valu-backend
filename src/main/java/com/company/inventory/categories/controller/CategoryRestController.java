ed60e2324125d118685c2286e4dbf240bbd58027
Alejandro Quintero Alzate <alejandro.quintero@robotmade.us>
2025-03-25 22:17:36 -0500

    feature: uscar categoria por Id

--- src/main/java/com/company/inventory/categories/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/categories/controller/CategoryRestController.java
@@ -5,6 +5,7 @@ import com.company.inventory.categories.services.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,25 @@ public class CategoryRestController {
     @Autowired
     private ICategoryService categoryService;
 
+    /**
+     *  get all the categories
+     * @return
+     */
     @GetMapping("/categories")
     public ResponseEntity<CategoryResponseRest> searchCategories () {
         ResponseEntity<CategoryResponseRest> response = categoryService.search();
+        return response;
+    }
+
+    /**
+     * get categories by id
+     * @param id
+     * @return
+     */
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> searchCategoriesById (@PathVariable Long id) {
+
+        ResponseEntity<CategoryResponseRest> response = categoryService.searchById(id);
         return response;
     }
 }
