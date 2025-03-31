4d96036b2b34ec7418d6d66da5bf70d09f74b629
Alejandro Quintero Alzate <alejandro.quintero@robotmade.us>
2025-03-30 20:01:34 -0500

    feature: Servicio completo de crud en productos

---
+++ src/main/java/com/company/inventory/products/model/Product.java
@@ -0,0 +1,53 @@
+package com.company.inventory.products.model;
+
+import com.company.inventory.categories.model.Category;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name="product")
+public class Product implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -1573552508413183887L;
+    /**
+     *
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int account;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JsonIgnoreProperties ( {"hibernateLazyInitializer", "handler"})
+    private Category category;
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column( name ="picture", columnDefinition = "longblob")
+    private byte[] picture;
+
+
+
+}
---
+++ src/main/java/com/company/inventory/products/service/IProductService.java
@@ -0,0 +1,16 @@
+package com.company.inventory.products.service;
+
+import com.company.inventory.products.model.Product;
+import com.company.inventory.products.response.ProductResponseRest;
+import org.springframework.http.ResponseEntity;
+
+public interface IProductService {
+
+    public ResponseEntity<ProductResponseRest> save(Product product, Long categoryId);
+    public ResponseEntity<ProductResponseRest> searchById(Long id);
+    public ResponseEntity<ProductResponseRest> searchByName(String name);
+    public ResponseEntity<ProductResponseRest> deleteById(Long id);
+    public ResponseEntity<ProductResponseRest> search();
+    public ResponseEntity<ProductResponseRest> update(Product product, Long categoryId, Long id);
+
+}
